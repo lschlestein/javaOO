621a190c7f731245000106605fe0b317cd302669
lschlestein <103784532+lschlestein@users.noreply.github.com>
2024-05-10 11:54:31 +0000

    Instanciando a classe pessoa

--- Main.java
+++ Main.java
@@ -6,5 +6,9 @@ Outro detalhe bastante importante, é que o nome da classe, deve ser igual ao no
 public class Main {//Cria uma classe, com acesso público de nome Main
     public static void main(String[] args){// Cria um método main, com visibilidade publica, void é devido ao método não ter nenhum tipo de retorno
         System.out.println("Olá Mundo");//Imprime "Olá Mundo", já direciona o cursor para a próxima linha
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome = "Lucas";
+        pessoa.idade = 38;
+        System.out.println("Nome " + pessoa.nome + " e idade: " + pessoa.idade); 
     }
 }
