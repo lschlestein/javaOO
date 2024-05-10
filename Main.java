e7eb0707e33efa317975ea6d49b6449e112d7955
lschlestein <103784532+lschlestein@users.noreply.github.com>
2024-05-10 08:32:04 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 /*
 A palavra Static, deixa explícito que a classe Main, é estática. Isso significa que ela pode ser chamada, pela JVM, mesmo sem ter sido instanciada.
 Outro detalhe, é que a JVM, sempre irá executar, a classe Main, sendo por assim dizer, o ponto de partida para nossa aplicação.
+Outro detalhe bastante importante, é que o nome da classe, deve ser igual ao nome do arquivo .java, nesse caso Main.java
 */
 public class Main {//Cria uma classe, com acesso público de nome Main
     public static void main(String[] args){// Cria um método main, com visibilidade publica, void é devido ao método não ter nenhum tipo de retorno
