1f6df6c351d4a4f312c5321142f7e624c4a4cebc
lschlestein <103784532+lschlestein@users.noreply.github.com>
2024-05-10 08:42:38 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+/*
+Na linguagem orientada a objetos, podemos fazer a analogia da classe como sendo a forma ou molde. A instância de uma classe é o sairá dessa forma
+moldado pela classe.
+Em uma classe, temos os métodos que representam as ações que são executadas pela classe
+E os atributos representam o estado atual da classe.
+É importante, ter pra si o conceito da representação do mundo real, através das classes.
+Podemos representar praticamente qualquer coisa de nosso cotiano, através de uma classe.
+*/
+public class Pessoa {//Cria uma classe, com acesso público de nome Pessoa
+    public String nome;
+    public int idade;
+}
